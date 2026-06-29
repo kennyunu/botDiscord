13afa9b7f218e7a0f43023ec5f0ff3dcfa904545
kennyunu <kriveral@unal.edu.co>
2026-06-28 19:59:37 -0500

    Todos los comandos funcionales

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+WEATHER = os.getenv("OPEN_WEATHER_API")
 
 class MiBot(discord.Client):
     async def on_ready(self):
@@ -17,8 +18,20 @@ class MiBot(discord.Client):
         if message.content.startswith("!"):
             await self.manejar_comando(message)
 
+    def coordenadas(self, ciudad):
+        respuesta = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={ciudad}&limit=1&appid={WEATHER}")
+        datos = respuesta.json()
+        lat=datos[0]["lat"]
+        lon=datos[0]["lon"]
+        return lat, lon
+
     async def manejar_comando(self, message):
         comando = message.content.split()[0].lower()
+        argumento = message.content.split()[1:]
+        argumento = " ".join(argumento)  # por si la ciudad tiene dos palabras como "Buenos Aires"
+
+
+        
 
         if comando == "!hola":
             await message.channel.send(f"Hola {message.author.name}!")
@@ -32,6 +45,7 @@ class MiBot(discord.Client):
                 "`!trivia` — hace una pregunta en inglés\n"
                 "`!dog` — envia una imagen de perrito\n"
                 "`!advice` — consejo en inglés\n"
+                "`!clima` — descripcion del clima en inglés\n"
             )
         
         elif comando == "!chiste":
@@ -60,6 +74,17 @@ class MiBot(discord.Client):
             datos = respuesta.json()
             await message.channel.send(f"{datos['slip']['advice']}\n\n")
 
+        elif comando == "!clima":
+            lat, lon =self.coordenadas(argumento)
+            respuesta = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER}&units=metric")
+            datos = respuesta.json()
+            descripcion = datos['weather'][0]["description"]
+            temp = datos['main']['temp']
+            await message.channel.send(
+                f"**{argumento}**\n"
+                f"🌡️ Temperatura: {temp}°K\n"
+                f"☁️ {descripcion.capitalize()}"
+            )
         
         else:
             await message.channel.send(f"Comando `{comando}` no reconocido. Escribe `!ayuda`.")
--- prueba_api.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-respuesta = requests.get("https://api.adviceslip.com/advice")
-datos = respuesta.json()
-print(datos)
